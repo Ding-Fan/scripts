7a9d64d1ee50eae77db5246a3e7fdac40d72667f
Ding <ouud@fastmail.com>
2024-03-25 09:25:00 +0900

    Create youtube-share-link-cleaner.js

---
+++ youtube-share-link-cleaner.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name        YouTube Share Link Cleaner
+// @namespace   Violentmonkey Scripts
+// @match       https://www.youtube.com/watch*
+// @grant       none
+// @version     1.0
+// @author      DRagon Vase
+// @description Remove tracking query parameters from YouTube share links
+// @license     MIT
+// @downloadURL https://update.greasyfork.org/scripts/490789/YouTube%20Share%20Link%20Cleaner.user.js
+// @updateURL https://update.greasyfork.org/scripts/490789/YouTube%20Share%20Link%20Cleaner.meta.js
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Attach listeners when the page loads
+  window.addEventListener("load", () => {
+    attachShareButtonListener();
+    observeDocumentForChanges();
+  });
+
+  function attachShareButtonListener() {
+    const shareButton = document.querySelector("ytd-menu-renderer");
+    if (shareButton) {
+      // Ensure we don't attach multiple listeners
+      shareButton.removeEventListener("click", initiateCleanProcess);
+      shareButton.addEventListener("click", initiateCleanProcess);
+    }
+  }
+
+  // Start the process to clean the URL after the share button is clicked
+  function initiateCleanProcess() {
+    // Set an interval to periodically check the dialog's visibility and clean the URL
+    const intervalId = setInterval(() => {
+      const dialog = document.querySelector("tp-yt-paper-dialog");
+      const inputField = document.querySelector(
+        "ytd-unified-share-panel-renderer input#share-url"
+      );
+
+      if (dialog && window.getComputedStyle(dialog).display !== "none") {
+        if (inputField && inputField.value) {
+          cleanShareUrl(inputField);
+        }
+      } else {
+        // If the dialog is not displayed, stop checking
+        clearInterval(intervalId);
+      }
+    }, 500); // Check every 500ms
+  }
+
+  // Cleans the URL in the share input field
+  function cleanShareUrl(inputField) {
+    let cleanUrl = new URL(inputField.value);
+    cleanUrl.searchParams.delete("si"); // Remove the 'si' parameter
+    inputField.value = cleanUrl.href;
+  }
+
+  // Reattach the share button listener when navigating to a new video
+  function observeDocumentForChanges() {
+    const observer = new MutationObserver(attachShareButtonListener);
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  }
+})();
